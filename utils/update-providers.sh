7a2679625751142aa8c23fad1c4f14a4bf30b086
Melvin Keskin <melvo@olomono.de>
2021-12-28 10:23:44 +0100

    utils: Add script for updating provider list

---
+++ utils/update-providers.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Kaidan - A user-friendly XMPP client for every device!
+#
+# Copyright (C) 2016-2022 Kaidan developers and contributors
+# (see the LICENSE file for a full list of copyright authors)
+#
+# Kaidan is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# In addition, as a special exception, the author of Kaidan gives
+# permission to link the code of its release with the OpenSSL
+# project's "OpenSSL" library (or with modified versions of it that
+# use the same license as the "OpenSSL" library), and distribute the
+# linked executables. You must obey the GNU General Public License in
+# all respects for all of the code used other than "OpenSSL". If you
+# modify this file, you may extend this exception to your version of
+# the file, but you are not obligated to do so.  If you do not wish to
+# do so, delete this exception statement from your version.
+#
+# Kaidan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Kaidan.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# This script updates the provider list used for registration.
+#
+# It should be run before each new release.
+#
+
+SOURCE_URL="https://invent.kde.org/melvo/xmpp-providers/-/jobs/artifacts/master/raw/providers-B.json?job=filtered-provider-lists"
+KAIDAN_SOURCES=$(dirname "$(readlink -f "${0}")")/..
+DESTINATION_PATH="${KAIDAN_SOURCES}/data/providers.json"
+
+curl -L ${SOURCE_URL} > ${DESTINATION_PATH}
