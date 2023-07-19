cfd4deffc9e91a502ca772d2a9d99cac9dac0aaf
Melvin Keskin <melvo@olomono.de>
2023-07-19 16:58:42 +0200

    RosterModel: Show 'Yesterday' for last messages instead of day of the week

--- src/RosterModel.cpp
+++ src/RosterModel.cpp
@@ -729,16 +729,19 @@ int RosterModel::positionToMove(int currentIndex)
 
 QString RosterModel::formattedLastMessageDateTime(const QDateTime &lastMessageDateTime) const
 {
-	const QDateTime &lastMessageDateTimeLocal { lastMessageDateTime.toLocalTime() };
+	const QDateTime &lastMessageLocalDateTime { lastMessageDateTime.toLocalTime() };
 
-	if (const auto now = QDateTime::currentDateTimeUtc(); lastMessageDateTime.date() == now.date()) {
+	if (const auto elapsedNightCount = lastMessageDateTime.daysTo(QDateTime::currentDateTimeUtc()); elapsedNightCount == 0) {
 		// Today: Return only the time.
-		return QLocale::system().toString(lastMessageDateTimeLocal.time(), QStringLiteral("hh:mm"));
-	} else if (lastMessageDateTime.daysTo(now) <= 7) {
+		return QLocale::system().toString(lastMessageLocalDateTime.time(), QStringLiteral("hh:mm"));
+	} else if (elapsedNightCount == 1) {
+		// Yesterday: Return that term.
+		return tr("Yesterday");
+	} else if (elapsedNightCount <= 7) {
 		// Between yesterday and seven days before today: Return the day of the week.
-		return QLocale::system().toString(lastMessageDateTimeLocal.date(), QStringLiteral("ddd"));
+		return QLocale::system().toString(lastMessageLocalDateTime.date(), QStringLiteral("ddd"));
 	} else {
 		// Older than seven days before today: Return the date.
-		return QLocale::system().toString(lastMessageDateTimeLocal.date(), QLocale::ShortFormat);
+		return QLocale::system().toString(lastMessageLocalDateTime.date(), QLocale::ShortFormat);
 	}
 }
