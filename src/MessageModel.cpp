e997343923f52a3cab498dec89dcb0d4de8bf230
Melvin Keskin <melvo@olomono.de>
2024-12-20 12:19:53 +0100

    MessageModel: Fix using correct iterators

--- src/MessageModel.cpp
+++ src/MessageModel.cpp
@@ -933,7 +933,7 @@ void MessageModel::removeMessage(const QString &messageId)
         return message.relevantId() == messageId;
     };
 
-    const auto itr = std::find_if(m_messages.begin(), m_messages.end(), hasCorrectId);
+    const auto itr = std::find_if(m_messages.cbegin(), m_messages.cend(), hasCorrectId);
 
     // Update the roster item of the current chat.
     if (itr != m_messages.cend()) {
