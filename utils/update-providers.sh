9e14c471ef39b8ed47414dfaed3920eb7cba29d3
Melvin Keskin <melvo@olomono.de>
2025-02-07 15:28:42 +0100

    update-providers: Loop through provider files and print modified ones

--- utils/update-providers.sh
+++ utils/update-providers.sh
@@ -9,18 +9,26 @@
 # This script updates the provider lists used for registration and autocomplete.
 #
 # It should be run before each new release.
+# It prints the modified files.
 #
 
 XMPP_PROVIDERS_VERSION=2
 XMPP_PROVIDERS_ROOT_URL="https://data.xmpp.net/providers/v${XMPP_PROVIDERS_VERSION}"
-KAIDAN_SOURCES=$(dirname "$(readlink -f "${0}")")/..
+KAIDAN_ROOT_DIRECTORY=$(dirname "$(readlink -f "${0}")")/..
+TARGET_DIRECTORY="${KAIDAN_ROOT_DIRECTORY}/data"
+PROVIDER_FILE_NAMES=(
+    "providers-B providers"
+    "providers-Ds providers-completion"
+)
 
-curl \
-	-L \
-	"${XMPP_PROVIDERS_ROOT_URL}/providers-B.json" \
-	> "${KAIDAN_SOURCES}/data/providers.json"
+for provider_file_name in "${PROVIDER_FILE_NAMES[@]}"
+do
+    source_provider_file_name=$(echo "$provider_file_name" | awk '{print $1}')
+    source_provider_file_url="${XMPP_PROVIDERS_ROOT_URL}/${source_provider_file_name}.json"
 
-curl \
-	-L \
-	"${XMPP_PROVIDERS_ROOT_URL}/providers-Ds.json" \
-	> "${KAIDAN_SOURCES}/data/providers-completion.json"
+    target_provider_file_name=$(echo "$provider_file_name" | awk '{print $2}')
+    target_provider_file_path="${TARGET_DIRECTORY}/${target_provider_file_name}.json"
+
+    curl -s -L "${source_provider_file_url}" > "${target_provider_file_path}"
+    git diff --name-only "${target_provider_file_path}"
+done
